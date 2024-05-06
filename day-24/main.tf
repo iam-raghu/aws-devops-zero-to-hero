ee7287117a08b40b25ffb36075e04d724f5235e9
Raghunath Kushawaha <78621381+iam-raghu@users.noreply.github.com>
2024-05-06 11:37:08 +0530

    Added terraform files for day 24

--- day-24/main.tf
+++ day-24/main.tf
@@ -2,17 +2,17 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
 
-resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "sub2" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1b"
   map_public_ip_on_launch = true
 }
 
@@ -30,75 +30,83 @@ resource "aws_route_table" "RT" {
 }
 
 resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
+  subnet_id = aws_subnet.subnet1.id
   route_table_id = aws_route_table.RT.id
 }
 
 resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.sub2.id
+  subnet_id = aws_subnet.subnet2.id
   route_table_id = aws_route_table.RT.id
 }
 
 resource "aws_security_group" "webSg" {
-  name   = "web"
-  vpc_id = aws_vpc.myvpc.id
-
+  name        = "web"
+  vpc_id      = aws_vpc.myvpc.id
+  
   ingress {
     description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Web-sg"
+    Name = "webSg"
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "abhisheksterraform2023project"
+resource "aws_s3_bucket" "may-6th-2024" {
+  bucket = "may-6th-2024"
 }
 
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
+  ami = "ami-05e00961530ae1b55"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub1.id
-  user_data              = base64encode(file("userdata.sh"))
+  subnet_id = aws_subnet.subnet1.id
+  user_data = base64encode(file("userdata.sh"))
+
+  tags = {
+    Name = "webserver1"
+  }
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
+  ami = "ami-05e00961530ae1b55"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub2.id
-  user_data              = base64encode(file("userdata1.sh"))
+  subnet_id = aws_subnet.subnet2.id
+  user_data = base64encode(file("userdata1.sh"))
+
+  tags = {
+    Name = "webserver2"
+  }
 }
 
-#create alb
+#Create Appliication Load Balancers
 resource "aws_lb" "myalb" {
-  name               = "myalb"
-  internal           = false
+  name = "myalb"
+  internal = false
   load_balancer_type = "application"
 
   security_groups = [aws_security_group.webSg.id]
-  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+  subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   tags = {
     Name = "web"
@@ -106,10 +114,10 @@ resource "aws_lb" "myalb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "myTG"
-  port     = 80
+  name = "myTG"
+  port = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.myvpc.id
 
   health_check {
     path = "/"
@@ -119,24 +127,24 @@ resource "aws_lb_target_group" "tg" {
 
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.webserver1.id
-  port             = 80
+  target_id = aws_instance.webserver1.id
+  port = 80
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.webserver2.id
-  port             = 80
+  target_id = aws_instance.webserver2.id
+  port = 80
 }
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.myalb.arn
-  port              = 80
-  protocol          = "HTTP"
+  port = 80
+  protocol = "HTTP"
 
   default_action {
     target_group_arn = aws_lb_target_group.tg.arn
-    type             = "forward"
+    type = "forward"
   }
 }
 
